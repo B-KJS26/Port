947705a5c0d56fc867ac1b28cffb492bdd005f7c
KJS <digyrh456789@gmail.com>
2023-03-08 22:34:03 +0900

    UPDATE :: 메인화면 수정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,18 +1,22 @@
 import styled from 'styled-components';
 import Ligradient from '../../images/111.png';
 export default function Main() {
-    return(
+    return (
         <Maining>
-            <DecoBox></DecoBox>
-            <Title>KIM HyeonJe</Title>
+            <DecoLine></DecoLine>
+            <Title>MINJUN MAN</Title>
             <Subtitle>Frontend Developer</Subtitle>
-            <DecoBox2></DecoBox2>
+            <Morebutton>watch more</Morebutton>
+            <DecoBox></DecoBox>
+            <Circlebg></Circlebg>
         </Maining>
     );
 }
+
+
 const Maining = styled.div`
     position: absolute;
-    height: 930px;
+    height: 100vh;
     width: 100%;
     top: 0;
     text-align: left;
@@ -21,36 +25,68 @@ const Maining = styled.div`
     background-size: 100% 100%;
     background-repeat: no-repeat;
 `
+const DecoLine = styled.div`
+    position: absolute;
+    width: 40vw;
+    height: 0.5vh;
+    background-color: white;
+    margin-left: 10vw;
+    margin-top: 13vh;
+    @media (max-width: 100px) {
+        width: 10px;
+        height: 10px;
+    }
+`
 const DecoBox = styled.div`
     position: absolute;
-    width: 25px;
-    height: 25px;
+    width: 1.5vw;
+    height: 3vh;
     background-color: white;
-    margin-left: 100px;
-    margin-top: 100px;
-`
-const DecoBox2 = styled.div`
-    position: absolute;
-    width: 25px;
-    height: 25px;
-    background-color: white;
-    margin-left: 100px;
-    margin-top: 805px;
+    margin-left: 10vw;
+    margin-top: 85vh;
 `
 const Title = styled.h1`
     position: absolute;
-    margin-top: 280px;  
-    margin-left: 100px; 
+    margin-top: 36vh;  
+    margin-left: 10vw; 
     font-family: Nexa;
-    font-size: 130px;
+    font-size: 6rem;
     color: white;
 `
-const Subtitle = styled.p`
+const Subtitle = styled.h1`
     position: absolute;
-    margin-top: 450px;
-    margin-left: 100px; 
-    font-family: NexaLight;
-    font-size: 70px;
+    margin-top: 48vh;
+    margin-left: 10.2vw; 
+    font-family: Nexabold;
+    font-size: 2rem;
     color: white;
+`
+const Circlebg = styled.div`
+    position: absolute;
+    width: 25vw;
+    height: 72vh;
+    margin-left: 60vw;
+    margin-top: 14vh;
+    background: rgba(255, 255, 255, 0.49);
+    mix-blend-mode: normal;
+    box-shadow: 20px 20px 20px 5px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(25px);
+    border-radius: 30px;
+`
 
+const Morebutton = styled.button`
+    position: absolute;
+    margin-left: 10vw;
+    margin-top: 64vh;
+    height: 6vh;
+    width: 12vw;
+    border: 4px;
+    border-style: solid;
+    border-color: white;
+    border-radius: 3rem;
+    background-color: #ffffff;
+    background-color: rgba( 255, 255, 255, 0);
+    color: white;
+    font-family: Nexaheavy;
+    font-size: 1.8rem;
 `
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 import Nexa from '../fonts/Nexa-Heavy.ttf';
 import Nexalight from '../fonts/Nexa-ExtraLight.ttf';
+import Nexablack from '../fonts/Nexa-Trial-Black.ttf';
+import Nexabold from '../fonts/Nexa-Trial-Bold.ttf';
+import Nexaheavy from '../fonts/Nexa-Trial-Heavy.ttf';
 const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'Nexa';
@@ -11,6 +14,21 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'NexaLight';
     font-style: normal;
     src: url(${Nexalight}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Nexablack';
+    font-style: normal;
+    src: url(${Nexablack}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Nexabold';
+    font-style: normal;
+    src: url(${Nexabold}) format('truetype');
+  }
+  @font-face {
+    font-family: 'Nexaheavy';
+    font-style: normal;
+    src: url(${Nexaheavy}) format('truetype');
   }
 `
 export default GlobalStyle; 
